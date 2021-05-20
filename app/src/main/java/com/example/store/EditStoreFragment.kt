718d9101f5f8c974645b14ccddef9dddcaaf0dcf
Calebji <cajimenez@ikeasistencia.com>
2021-05-20 18:07:33 -0500

    Fragment

---
+++ app/src/main/java/com/example/store/EditStoreFragment.kt
@@ -0,0 +1,20 @@
+package com.example.store
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.store.databinding.FragmentEditStoreBinding
+
+class EditStoreFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentEditStoreBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        mBinding = FragmentEditStoreBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+}
