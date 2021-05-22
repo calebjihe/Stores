5c61b4aa7c1dcf829408f3105dd41939a3ddaf28
Calebji <cajimenez@ikeasistencia.com>
2021-05-21 20:00:21 -0500

    Ocultar el teclado y regresar al menu principal

--- app/src/main/java/com/example/store/EditStoreFragment.kt
+++ app/src/main/java/com/example/store/EditStoreFragment.kt
@@ -1,7 +1,9 @@
 package com.example.store
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.store.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
@@ -47,10 +49,13 @@ class EditStoreFragment : Fragment() {
                 doAsync {
                     StoreApplication.databse.storeDao().addStore(store)
                     uiThread {
+                        hideKeyboard()
+
                         Snackbar.make(mBinding.root,
                             getString(R.string.edit_store_message_save_success),
                             Snackbar.LENGTH_SHORT)
                             .show()
+                        mActivity?.onBackPressed()
                     }
                 }
                 true
@@ -58,6 +63,20 @@ class EditStoreFragment : Fragment() {
             else -> return super.onOptionsItemSelected(item)
         }
         //return super.onOptionsItemSelected(item)
+
+    }
+
+    private fun  hideKeyboard(){
+        val imm  = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(view != null){
+            imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        hideKeyboard()
+        super.onDestroyView()
     }
 
     override fun onDestroy() {
